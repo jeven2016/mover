18d3cff8123ae9069548fa0c02c852d695b50166
jujucom <jujucom@126.com>
2022-05-21 23:57:08 +0800

    initial commit

--- main/main.go
+++ main/main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"github.com/duke-git/lancet/v2/convertor"
-	"mover/src/common"
+	common2 "mover/common"
 )
 
-var log = common.Log
+var log = common2.Log
 
 func main() {
 	log.Infoln("Mover program started")
-	if setting, err := common.GetParams(); err != nil {
+	if setting, err := common2.GetParams(); err != nil {
 		log.WithError(err).Error("Program Exited:")
 	} else {
 		str := convertor.ToString(*setting)
 		log.Infoln("the arguments parsed:", str)
-		common.Detect(*setting)
+		common2.Detect(*setting)
 		log.Infoln("Program started")
 		select {}
 	}
